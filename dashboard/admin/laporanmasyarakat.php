f92d448bb0cb38eeecf684b9bd3ffcca6a06ab95
khairul <khairuldocument14@gmail.com>
2022-11-16 22:12:35 +0800

    Update Ke 2

--- dashboard/admin/laporanmasyarakat.php
+++ dashboard/admin/laporanmasyarakat.php
@@ -316,7 +316,7 @@
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
                     <a class="btn btn-primary" href="../../app/function.php?logout">Logout</a>
                 </div>
-            </div>
+            </div>  
         </div>
     </div>
 
